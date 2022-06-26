91687035f2295a5fa75c3bb5464942dd15b1ae7e
Gegzz <93631562+Gegzz@users.noreply.github.com>
2022-06-26 15:27:50 +0400

    Css

--- tictactoe2/src/Components/ResetButton.js
+++ tictactoe2/src/Components/ResetButton.js
@@ -4,7 +4,7 @@ import "./ResetButton.css";
 const ResetButton = ({ resetBoard }) => {
   return (
     <button className="reset-btn" onClick={resetBoard}>
-      Reset
+      Restart
     </button>
   );
 };
